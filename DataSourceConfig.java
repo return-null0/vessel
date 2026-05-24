4ad3af250970be5754beb4c6b144ed543b03ea0a
renaldo <renaldohyacinthe@gmail.com>
2026-05-24 16:17:25 -0400

    spring setup

--- DataSourceConfig.java
+++ DataSourceConfig.java
@@ -5,19 +5,27 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.jdbc.datasource.DriverManagerDataSource;
 import javax.sql.DataSource;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 @Configuration
 public class DataSourceConfig {
 
+    private final ClusterConfig clusterConfig;
+
+    public DataSourceConfig(ClusterConfig clusterConfig) {
+        this.clusterConfig = clusterConfig;
+    }
+
     @Bean
     public DataSource dataSource() {
         DataSourceRouter router = new DataSourceRouter();
         Map<Object, Object> targetDataSources = new HashMap<>();
+        List<String> activeIps = clusterConfig.getActiveShardIps();
 
-        targetDataSources.put("shard1", createDataSource("10.0.0.2"));
-        targetDataSources.put("shard2", createDataSource("10.0.0.3"));
-        targetDataSources.put("shard3", createDataSource("10.0.0.4"));
+        for (int i = 0; i < activeIps.size(); i++) {
+            targetDataSources.put("shard" + (i + 1), createDataSource(activeIps.get(i)));
+        }
 
         router.setTargetDataSources(targetDataSources);
         router.setDefaultTargetDataSource(targetDataSources.get("shard1"));
@@ -28,7 +36,7 @@ public class DataSourceConfig {
     private DataSource createDataSource(String ipAddress) {
         DriverManagerDataSource dataSource = new DriverManagerDataSource();
         dataSource.setDriverClassName("org.mariadb.jdbc.Driver");
-        dataSource.setUrl("jdbc:mariadb://" + ipAddress + ":3306/mysql");
+        dataSource.setUrl("jdbc:mariadb://" + ipAddress + ":3306/appdata");
         dataSource.setUsername("mysql");
         dataSource.setPassword("vesseladmin");
         return dataSource;
--- vessel-engine/src/main/java/me/renaldohyacinthe/vessel_engine/VesselEngineApplication.java
+++ vessel-engine/src/main/java/me/renaldohyacinthe/vessel_engine/VesselEngineApplication.java
@@ -2,12 +2,12 @@ package me.renaldohyacinthe.vessel_engine;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 
-@SpringBootApplication
+// This stops Spring from trying to configure a database pool automatically
+@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class})
 public class VesselEngineApplication {
-
-	public static void main(String[] args) {
-		SpringApplication.run(VesselEngineApplication.class, args);
-	}
-
+    public static void main(String[] args) {
+        SpringApplication.run(VesselEngineApplication.class, args);
+    }
 }
